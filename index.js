e75acd517a3551b9674c76ad8e0176942dd3b3d5
Imran Nur Hirey <95400512+imranhirey@users.noreply.github.com>
2022-08-18 11:01:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ app.get('/',(req,res)=>{
     res.send('<h1>this is secure nginx server with load blancer</h1>');
 }
 );
-app.listen(3000,()=>{
-    console.log('Server started at port 3000');
+app.listen(4200,()=>{
+    console.log('Server started at port 4200');
 }
 );
